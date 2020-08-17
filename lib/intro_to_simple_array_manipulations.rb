669acd2b74c57ee3790efb778916a4ad2c526206
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-17 14:02:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,9 @@
 def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push(next_color)
 end
+def using_unshift(bouroughs_in_nyc)
+  bouroughs_in_nyc.unshift()
+end
+def using_pop(array)
+  array.pop
+end
